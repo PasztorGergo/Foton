66a3edab62f30fc8e86e39bdcf86ac8bef4e092d
PasztorGergo <pasztorg05@gmail.com>
2022-10-22 16:34:57 +0200

    document-fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Footer, Navbar, SideBar } from "../components";
-import { Head } from "next/document";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
